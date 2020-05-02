488056116fae89e27432cfb645749584816c5847
andieape <andie.ape@gmail.com>
2020-05-02 18:00:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -98,7 +98,52 @@ var buttons = new Nexus.Piano('#Keyboard', {
 Tone.context.latencyHint = 'interactive';
 
 var pianO = new Tone.Sampler({
-	
+
+/*CLOSE GRAND*/
+  /*      'A1': 'A1.mp3',
+        'A#1': 'As1.mp3',
+        'B1': 'B1.mp3',
+        'C1': 'C1.mp3',
+        'C#1': 'Cs1.mp3',
+        'D1': 'D1.mp3',
+        'D#1': 'Ds1.mp3',
+        'E1': 'E1.mp3',
+        'F1': 'F1.mp3',
+        'F#1': 'Fs1.mp3',
+        'G1': 'G1.mp3',
+        'G#1': 'Gs1.mp3'
+
+*/
+   /*     'A0' : 'A0.[mp3|ogg]',
+        'C1' : 'C1.[mp3|ogg]',
+        'D#1' : 'Ds1.[mp3|ogg]',
+        'F#1' : 'Fs1.[mp3|ogg]',
+        'A1' : 'A1.[mp3|ogg]',
+        'C2' : 'C2.[mp3|ogg]',
+        'D#2' : 'Ds2.[mp3|ogg]',
+        'F#2' : 'Fs2.[mp3|ogg]',
+        'A2' : 'A2.[mp3|ogg]',
+        'C3' : 'C3.[mp3|ogg]',
+        'D#3' : 'Ds3.[mp3|ogg]',
+        'F#3' : 'Fs3.[mp3|ogg]',
+        'A3' : 'A3.[mp3|ogg]',
+        'C4' : 'C4.[mp3|ogg]',
+        'D#4' : 'Ds4.[mp3|ogg]',
+        'F#4' : 'Fs4.[mp3|ogg]',
+        'A4' : 'A4.[mp3|ogg]',
+        'C5' : 'C5.[mp3|ogg]',
+        'D#5' : 'Ds5.[mp3|ogg]',
+        'F#5' : 'Fs5.[mp3|ogg]',
+        'A5' : 'A5.[mp3|ogg]',
+        'C6' : 'C6.[mp3|ogg]',
+        'D#6' : 'Ds6.[mp3|ogg]',
+        'F#6' : 'Fs6.[mp3|ogg]',
+        'A6' : 'A6.[mp3|ogg]',
+        'C7' : 'C7.[mp3|ogg]',
+        'D#7' : 'Ds7.[mp3|ogg]',
+        'F#7' : 'Fs7.[mp3|ogg]',
+        'A7' : 'A7.[mp3|ogg]',
+        'C8' : 'C8.[mp3|ogg]'*/
 
     /* old */
     'A0': 'A0.[mp3|ogg]',
@@ -195,7 +240,7 @@ var pianO = new Tone.Sampler({
     attack: 0,
     release: 3,
     sustain: 1,
-    decay: 3    
+    decay: 1    
 });
 
 /* KILLING NOISES HERE */
@@ -230,18 +275,15 @@ pianO.toMaster();
 //comp.toMaster();
 
 
-
-
-
 buttons.on('change', function(note) {
     
-    console.log(Tone.Frequency(note.note).toNote());
+   
     if (note.state === true) {
         pianO.triggerAttack(Tone.Frequency(note.note, 'midi').toNote());
         
     } else if (note.state === false) {
         
-        pianO.triggerRelease(Tone.Frequency(note.note).toNote());
+        pianO.triggerRelease(Tone.Frequency(note.note, "midi").toNote());
     }
 });
 
@@ -322,10 +364,6 @@ function resizePiano(x) {
 //reordering the obj to set the ids
 var keyIds = Object.values(keyMap).sort(function(a, b){return a-b});;
 
-
-
-console.log(keyIds);
-
 $(window).on('load', function(){
     console.log('ready');
     var keyB = $('#Keyboard').find('rect');
@@ -340,8 +378,3 @@ $(window).on('load', function(){
     }
   
 });
-
-
-
-
-
